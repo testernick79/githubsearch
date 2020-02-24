d544254d741302559e92f733ba4e202e50b32fc4
nick <123@gmail.com>
2020-02-24 09:14:57 -0800

    added client id and client client secret for git hub for multiple requests

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  async componentDidMount() {
+    this.setState( { loading: true } );
+
+    const res = await axios.get( `https://api.github.com/users?client_id=$
+    {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}` );
+
+    this.setState( { users: res.data, loading: false } )
+
+    //console.log( res.data );
+
+  }
 
   render() {
     return (
       <div className="App">
 
-        <Navbar title='Github Search' />
-        <UserItem />
+        <Navbar />
+        <div className='container'>
+          <Users loading={ this.state.loading } users={ this.state.users } />
+        </div>
       </div>
 
     );
   }
 }
-
 
 export default App;
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,34 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class UserItem extends Component {
-    state = {
+const UserItem = ( props ) => {
 
-        id: 'id',
-        login: 'mojombo',
-        html_url: 'https://github.com/mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        reposr_url: 'https://api.github.com/users/mojombo/repos'
-
-
-    };
-
-
-
-    render() {
-        const { avatar_url, login, html_url, reposr_url } = this.state;
-        return (
-            <div className='card text-center'>
-                <img src={ avatar_url } alt='' className='round-img' style={ { width: '90px' } } />
-                <h3>{ login }</h3>
-                <div>
-                    <a href={ html_url } className='btn btn-dark btn-sm my-1'>Github</a>
-                </div>
-                <div>
-                    <a href={ reposr_url } className='btn btn-dark btn-sm my-1'>View Repos</a>
-                </div>
+    const { avatar_url, login, html_url, reposr_url } = props.user;
+    return (
+        <div className='card text-center'>
+            <img src={ avatar_url } alt='' className='round-img' style={ { width: '90px' } } />
+            <h3>{ login }</h3>
+            <div>
+                <a href={ html_url } className='btn btn-dark btn-sm my-1'>Github</a>
             </div>
-        );
-    }
+            <div>
+                <a href={ reposr_url } className='btn btn-dark btn-lg my-1'>View Repos</a>
+            </div>
+        </div>
+    );
+
 }
 
 
